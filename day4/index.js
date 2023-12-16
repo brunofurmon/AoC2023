0644c029ce7b9fafa2aae48ff99ae9d1b93e59e1
Bruno Furtado <brunofurmon@gmail.com>
2023-12-16 17:00:39 -0300

    Add Day4 part2

--- day4/index.js
+++ day4/index.js
@@ -7,32 +7,68 @@ const lines = readline.createInterface({
   crlfDelay: Infinity
 });
 
+const scratchCards = {};
+
 /**
  * 
  * @param {string} line
  * @returns {number}
  */
-const processLine = (line) => {
-  const numbers = line.split(':')[1].trim().split(' | ');
+
+let globalProcess = 0;
+
+const processCard = (cardNumber) => {
+  if (cardNumber > Object.keys(scratchCards).length) {
+    // console.log('Breaking on out of bounds');
+    return;
+  }
+  globalProcess += 1;
+  const scratchCard = scratchCards[cardNumber][0];
+  // console.log(scratchCard);
+  const numbers = scratchCard.split(':')[1].trim().split(' | ');
   const winningNumbers = numbers[0].split(' ').filter(str => str !== '').map(n => parseInt(n.trim()));
   const cardNumbers = numbers[1].split(' ').filter(str => str !== '').map(n => parseInt(n.trim()));
 
-  console.log(winningNumbers);
-  console.log(cardNumbers);
+  // console.log(winningNumbers);
+  // console.log(cardNumbers);
 
-  return cardNumbers.reduce((acc, cur) => {
-    if (winningNumbers.includes(cur)) {
-      acc = (acc === 0) ? 1 : acc *= 2;
-      console.log(`Found ${cur} in ${winningNumbers}. Current points: ${acc}`);
-    }
-    return acc;
-  }, 0);
+  let matches;
+  // console.log(scratchCards[cardNumber]);
+  if (scratchCards[cardNumber][1]) {
+    matches = scratchCards[cardNumber][1];
+  } else {
+    matches = cardNumbers.reduce((acc, cur) => {
+      if (winningNumbers.includes(cur)) {
+        acc += 1;
+      }
+      return acc;
+    }, 0);
+    scratchCards[cardNumber][1] = matches;
+  }
+
+  // console.log(`Processing card ${cardNumber} - ${matches} matches`);
+
+  if (matches === 0) {
+    // console.log('Breaking on 0 matches');
+    return;
+  }
+
+  for (let cardToProcess = cardNumber + 1; cardToProcess < cardNumber + 1 + matches; cardToProcess++) {
+    // console.log(`Adding card ${cardToProcess} to process`);
+    processCard(cardToProcess);
+  }
 };
 
 (async () => {
-  let points = 0;
+  let index = 0;
   for await (const line of lines) {
-    points += processLine(line);
+    scratchCards[index + 1] = [line, undefined];
+    index++;
   }
-  console.log(points);
+
+  for (let cardNumber = 1; cardNumber <= Object.keys(scratchCards).length; cardNumber++) {
+    processCard(cardNumber);
+  }
+  // console.log(globalProcess);
 })();
+
